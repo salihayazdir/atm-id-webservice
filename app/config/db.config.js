92eb59e4913d47d792b64b7264003148b21ee181
salihayazdir <salihayazdir@gmail.com>
2022-07-06 17:35:27 +0300

    sql config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,14 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "123321abC!",
-    DB: "ATM_DB"
-};
+    HOST: "us-cdbr-east-06.cleardb.net",
+    USER: "bbdaca0cf64a7b",
+    PASSWORD: "122031b7",
+    DB: "heroku_524e5f55e8d0951"
+  };
+
+// local sql
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "root",
+//     PASSWORD: "123321abC!",
+//     DB: "ATM_DB"
+// };
