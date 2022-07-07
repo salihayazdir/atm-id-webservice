56e501424074feea1d90069d646a08f713c24b40
salihayazdir <salihayazdir@gmail.com>
2022-07-07 08:50:01 +0300

    test8

--- routes/db-route.js
+++ routes/db-route.js
@@ -15,7 +15,7 @@ router.get('/', async (req, res) => {
       const client = await pool.connect();
       const result = await client.query('SELECT * FROM test_table');
       const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
+      res.json( results );
       client.release();
     } catch (err) {
       console.error(err);
