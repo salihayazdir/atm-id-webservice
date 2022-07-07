8f19bfe9c2fbc64e09efd9b722aadafff514fba2
salihayazdir <salihayazdir@gmail.com>
2022-07-07 07:52:26 +0300

    test5

---
+++ routes/db-route.js
@@ -0,0 +1,28 @@
+const { application } = require('express');
+const express = require('express')
+const router = express.Router()
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+app.use('/db')
+
+router.get('/', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  });
+  
+modeule.exports = router
--- server.js
+++ server.js
@@ -2,13 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+
 
 // var corsOptions = {
 //   origin: "http://localhost:8081"
@@ -18,10 +12,12 @@ const pool = new Pool({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.get("/", (req, res) => {
   res.json({ message: "Welcome" });
 });
+
+const db = require('./routes/db-route')
+app.use('./db', db)
 
 // const mysql = require("mysql2");
 
@@ -151,26 +147,13 @@ app.get("/", (req, res) => {
 //     });
 //   };  
 
-  var router = require("express").Router();
 //   app.use('/api/atms', router);
 
 //   router.post("/", create);
 //   router.get("/", findAll);
 //   router.delete("/", deleteAll);
 
-  router.get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  });
-  
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
