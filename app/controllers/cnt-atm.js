313a18101c29c1f150ee11eab84dacf5c5ccf39f
salihayazdir <salihayazdir@gmail.com>
2022-07-08 19:48:36 +0300

    test11

--- app/controllers/cnt-atm.js
+++ app/controllers/cnt-atm.js
@@ -1,43 +1,43 @@
 const db = require('../db')
 
-const atmAddNew = async (req, res) => {
-    if (!req.body) {
-      res.status(400).send({
-        message: "Content can not be empty."
-      });
-    }
+// const atmAddNew = async (req, res) => {
+//     if (!req.body) {
+//       res.status(400).send({
+//         message: "Content can not be empty."
+//       });
+//     }
     
-    const newAtm = {
-        MemberNo: req.body.MemberNo,
-        AtmReferenceCode: req.body.AtmReferenceCode,
-    }
+//     const newAtm = {
+//         MemberNo: req.body.MemberNo,
+//         AtmReferenceCode: req.body.AtmReferenceCode,
+//     }
 
-    try {
-      const client = await db.connect();
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.json( results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  }
+//     try {
+//       const client = await db.connect();
+//       const result = await client.query('SELECT * FROM atmunits');
+//       const results = { 'results': (result) ? result.rows : null};
+//       res.json( results );
+//       client.release();
+//     } catch (err) {
+//       console.error(err);
+//       res.send("Error " + err);
+//     }
+//   }
 
-module.exports = {
-    atmAddNew,
-}
+  
+  const atmAddNew = async (req, res) => {
+        try {
+            const client = await pool.connect();
+            const result = await client.query('SELECT * FROM test_table');
+            const results = { 'results': (result) ? result.rows : null};
+            res.json( results );
+            client.release();
+          } catch (err) {
+              console.error(err);
+              res.send("Error " + err);
+            }
+          }
 
-
-  // const atmAddNew = async (req, res) => {
-  //     try {
-  //       const client = await pool.connect();
-  //       const result = await client.query('SELECT * FROM test_table');
-  //       const results = { 'results': (result) ? result.rows : null};
-  //       res.json( results );
-  //       client.release();
-  //     } catch (err) {
-  //       console.error(err);
-  //       res.send("Error " + err);
-  //     }
-  //   }
+        module.exports = {
+            atmAddNew,
+        }
