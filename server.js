e1a894c354cca861317867e2a919b316546ff67a
salihayazdir <salihayazdir@gmail.com>
2022-07-11 19:16:51 +0300

    cors config

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-// var corsOptions = { origin: "http://localhost:8081" };
-// app.use(cors(corsOptions));
+var corsOptions = { origin: false };
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
