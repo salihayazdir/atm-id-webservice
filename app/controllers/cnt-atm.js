4b69b3772289525adfc991047f7c803f2a827400
salihayazdir <salihayazdir@gmail.com>
2022-07-18 22:04:57 +0300

    query string parameters configuration for get requests

--- app/controllers/cnt-atm.js
+++ app/controllers/cnt-atm.js
@@ -1,13 +1,56 @@
 const db = require('../config/db')
   
 const listAll = async (req, res) => {
+  const { atmreferencecode, atmname, licensetag, adress,
+    district, neighborhood, servicedependency, restrictedatm,
+    airportlocated, malllocated, universitylocated, depositflag, withdrawflag,
+    terminalcoinoperator, nfcflag, biometryflag, visuallyimpairedflag, orthopedicdisabledflag,
+    atmage, geocodelatitude, geocodelongitude
+  } = req.query
+  const conditions = [];
+  const values = [];
+
+  if (atmreferencecode) { conditions.push(`atmreferencecode`); values.push(atmreferencecode); }
+  if (atmname) { conditions.push(`atmname`); values.push(atmname); }
+  if (licensetag) { conditions.push(`licensetag`); values.push(licensetag); }
+  if (adress) { conditions.push(`adress`); values.push(adress); }
+  if (district) { conditions.push(`district`); values.push(district); }
+  if (neighborhood) { conditions.push(`neighborhood`); values.push(neighborhood); }
+  if (servicedependency) { conditions.push(`servicedependency`); values.push(servicedependency); }
+  if (restrictedatm) { conditions.push(`restrictedatm`); values.push(restrictedatm); }
+  if (airportlocated) { conditions.push(`airportlocated`); values.push(airportlocated); }
+  if (malllocated) { conditions.push(`malllocated`); values.push(malllocated); }
+  if (universitylocated) { conditions.push(`universitylocated`); values.push(universitylocated); }
+  if (depositflag) { conditions.push(`depositflag`); values.push(depositflag); }
+  if (withdrawflag) { conditions.push(`withdrawflag`); values.push(withdrawflag); }
+  if (terminalcoinoperator) { conditions.push(`terminalcoinoperator`); values.push(terminalcoinoperator); }
+  if (nfcflag) { conditions.push(`nfcflag`); values.push(nfcflag); }
+  if (biometryflag) { conditions.push(`biometryflag`); values.push(biometryflag); }
+  if (visuallyimpairedflag) { conditions.push(`visuallyimpairedflag`); values.push(visuallyimpairedflag); }
+  if (orthopedicdisabledflag) { conditions.push(`orthopedicdisabledflag`); values.push(orthopedicdisabledflag); }
+  if (atmage) { conditions.push(`atmage`); values.push(atmage); }
+  if (geocodelatitude) { conditions.push(`geocodelatitude`); values.push(geocodelatitude); }
+  if (geocodelongitude) { conditions.push(`geocodelongitude`); values.push(geocodelongitude); }
+
+  const paramQuery = conditions.map((condition, index) => {
+    return `${condition} = $${index + 1} ${(index + 1 === conditions.length) ? '' : 'AND '}`
+  })
+
   try {
     const client = await db.connect();
-    const result = await client.query('SELECT * FROM atmunits', 
+    client.query(
+      `SELECT * FROM atmunits ${(conditions.length) ? (`WHERE ${paramQuery.join('')}`) : ''}`,
+      values, 
       (error, results) => {
-        if (error) throw error;
-        res.json( results.rows );
+        try {
+          if (error) throw error;
+          res.json( results.rows );
+        } catch (err) {
+          console.error(err);
+          res.send("Error " + err);
+        }
       });
+      
     client.release();
   } 
   catch (err) {
@@ -15,6 +58,7 @@ const listAll = async (req, res) => {
     res.send("Error " + err);
   }
 }
+
 
 const getAtm = async (req, res) => {
   const id = parseInt(req.params.id);
@@ -129,59 +173,53 @@ const deleteAtm = async (req, res) => {
 }
 
 const editAtm = async (req, res) => {
-  const id = parseInt(req.params.id);
-  const { 
-    atmreferencecode, atmname, licensetag, adress, district,
-    neighborhood, servicedependency, restrictedatm, airportlocated,
-    malllocated, universitylocated, depositflag, withdrawflag, terminalcoinoperator, 
-    nfcflag, biometryflag, visuallyimpairedflag, orthopedicdisabledflag, atmage,
-    geocodelatitude, geocodelongitude
-  } = req.body;
-  const client = await db.connect();
-  try {
-    client.query('SELECT * FROM atmunits WHERE globalatmid = $1', [id], (error, results) => {
-      try {
-        if (error) throw error;
-        if (!results.rows.length) {
-          res.status(451).send('Hata. ATM Bulunamadı')
-        } else {
-          client.query(`
-            UPDATE atmunits SET 
-              atmreferencecode = $2, atmname = $3, licensetag = $4, adress = $5,
-              district = $6, neighborhood = $7, servicedependency = $8, restrictedatm = $9, airportlocated = $10,
-              malllocated = $11, universitylocated = $12, depositflag = $13, withdrawflag = $14, terminalcoinoperator = $15,
-              nfcflag = $16, biometryflag = $17, visuallyimpairedflag = $18, orthopedicdisabledflag = $19, atmage = $20,
-              geocodelatitude = $21, geocodelongitude = $22
-            WHERE globalatmid = $1`,
-            [
-              id, atmreferencecode, atmname, licensetag, adress,
-              district, neighborhood, servicedependency, restrictedatm, airportlocated,
-              malllocated, universitylocated, depositflag, withdrawflag, terminalcoinoperator,
-              nfcflag, biometryflag, visuallyimpairedflag, orthopedicdisabledflag, atmage,
-              geocodelatitude, geocodelongitude
-            ],
-            (error, results) => {
-            try {
-              if (error) throw error;
-              res.send(`${id} ID'li ATM başarıyla güncellendi.`)
-            }
-            catch (error) {
-              console.error(err);
-              res.send("Error " + err)
-            }
-          })
-        }
+const id = parseInt(req.params.id);
+const { 
+  atmreferencecode, atmname, licensetag, adress, district,
+  neighborhood, servicedependency, restrictedatm, airportlocated,
+  malllocated, universitylocated, depositflag, withdrawflag, terminalcoinoperator, 
+  nfcflag, biometryflag, visuallyimpairedflag, orthopedicdisabledflag, atmage,
+  geocodelatitude, geocodelongitude
+} = req.body;
+const client = await db.connect();
+  client.query('SELECT * FROM atmunits WHERE globalatmid = $1', [id], (error, results) => {
+    try {
+      if (error) throw error;
+      if (!results.rows.length) {
+        res.status(451).send('Hata. ATM Bulunamadı')
+      } else {
+        client.query(`
+          UPDATE atmunits SET 
+            atmreferencecode = $2, atmname = $3, licensetag = $4, adress = $5,
+            district = $6, neighborhood = $7, servicedependency = $8, restrictedatm = $9, airportlocated = $10,
+            malllocated = $11, universitylocated = $12, depositflag = $13, withdrawflag = $14, terminalcoinoperator = $15,
+            nfcflag = $16, biometryflag = $17, visuallyimpairedflag = $18, orthopedicdisabledflag = $19, atmage = $20,
+            geocodelatitude = $21, geocodelongitude = $22
+          WHERE globalatmid = $1`,
+          [
+            id, atmreferencecode, atmname, licensetag, adress,
+            district, neighborhood, servicedependency, restrictedatm, airportlocated,
+            malllocated, universitylocated, depositflag, withdrawflag, terminalcoinoperator,
+            nfcflag, biometryflag, visuallyimpairedflag, orthopedicdisabledflag, atmage,
+            geocodelatitude, geocodelongitude
+          ],
+          (error, results) => {
+          try {
+            if (error) throw error;
+            res.send(`${id} ID'li ATM başarıyla güncellendi.`)
+          }
+          catch (error) {
+            console.error(err);
+            res.send("Error " + err)
+          }
+        })
       }
-      catch {
-        console.error(err);
-        res.send("Error " + err)
-      }
-    })
-  }
-  catch (err) {
-    console.error(err);
-    res.send("Error " + err)
-  }
+    }
+    catch {
+      console.error(err);
+      res.send("Error " + err)
+    }
+  })
 }
 
 module.exports = {
